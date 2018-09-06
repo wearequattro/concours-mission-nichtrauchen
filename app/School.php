459c5e592d5dd0990e353cc088e16f608f3e89da
Gilles Braun <gilles@kirepo.lu>
2018-09-06 14:22:51 +0200

    Add Schools

---
+++ app/School.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class School
+ * @package App
+ *
+ * @property int id
+ * @property string name
+ * @property string address
+ * @property string postal_code
+ * @property string city
+ * @property Carbon updated_at
+ * @property Carbon created_at
+ *
+ * @method static School create(array $map)
+ */
+class School extends Model {
+
+    protected $fillable = [
+        'name',
+        'address',
+        'postal_code',
+        'city',
+    ];
+
+}
---
+++ database/seeds/SchoolSeeder.php
@@ -0,0 +1,307 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SchoolSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        $data = [
+            [
+                "ATERT-LYCEE REDANGE",
+                "1, rue du Lycée",
+                "L-8508",
+                "REDANGE-SUR-ATTERT"
+            ],
+            [
+                "ATHENEE DE LUXEMBOURG",
+                "24, boulevard Pierre Dupong",
+                "L-1430",
+                "LUXEMBOURG"
+            ],
+            [
+                "ECOLE EUROPEENNE LUXEMBOURG I",
+                "23, boulevard Konrad Adenauer",
+                "L-1115",
+                "LUXEMBOURG"
+            ],
+            [
+                "ECOLE EUROPEENNE LUXEMBOURG II",
+                "6, rue Gaston Thorn ",
+                "L-8268",
+                "BERTRANGE"
+            ],
+            [
+                "ECOLE INTERNATIONALE DE MONDORF-LES-BAINS",
+                "2, route de Bumerange",
+                "L-5659",
+                "MONDORF-LES-BAINS"
+            ],
+            [
+                "ECOLE INTERNATIONALE DIFFERDANGE",
+                "Campus de l'EIDE",
+                "L-4573",
+                "DIFFERDANGE"
+            ],
+            [
+                "ECOLE INTERNATIONALE ESCH",
+                "91, rue Victor Hugo",
+                "L-4141",
+                "ESCH-SUR-ALZETTE"
+            ],
+            [
+                "ECOLE PRIVEE FIELDGEN",
+                "21, rue d'Anvers",
+                "L-1130",
+                "LUXEMBOURG"
+            ],
+            [
+                "ECOLE PRIVEE MARIE-CONSOLATRICE",
+                "101, rue de Luxembourg",
+                "L-4221",
+                "ESCH-SUR-ALZETTE"
+            ],
+            [
+                "ECOLE PRIVEE NOTRE-DAME STE SOPHIE",
+                "4, rue Marguerite de Busbach",
+                "L-1269",
+                "LUXEMBOURG"
+            ],
+            [
+                "ECOLE PRIVEE SAINTE-ANNE",
+                "108, Grand-Rue",
+                "L-9051",
+                "ETTELBRUCK"
+            ],
+            [
+                "INTERNATIONAL SCHOOL LUXEMBOURG",
+                "36, boulevard Pierre Dupong",
+                "L-1430",
+                "LUXEMBOURG"
+            ],
+            [
+                "LENSTER LYCEE",
+                "2, rue Victor Ferrant",
+                "L-6122",
+                "JUNGLINSTER"
+            ],
+            [
+                "LYCEE ALINE MAYRISCH",
+                "38, boulevard Pierre Dupong",
+                "L-1430",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE BEL-VAL    ",
+                "100, avenue du Blues",
+                "L-4368",
+                "BELVAUX"
+            ],
+            [
+                "LYCEE CLASSIQUE DE DIEKIRCH",
+                "32, avenue de la Gare",
+                "L-9233",
+                "DIEKIRCH"
+            ],
+            [
+                "LYCEE CLASSIQUE DE DIEKIRCH - ANNEXE MERSCH",
+                "Square Princesse Marie Astrid",
+                "L-7523",
+                "MERSCH"
+            ],
+            [
+                "LYCEE CLASSIQUE D'ECHTERNACH",
+                "1, rue de Pont",
+                "L-6401",
+                "ECHTERNACH"
+            ],
+            [
+                "LYCEE DE GARCONS DE LUXEMBOURG",
+                "Place Auguste Laurent",
+                "L-1921",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE DE GARCONS ESCH",
+                "71, rue du Fossé",
+                "L-4123",
+                "ESCH-SUR-ALZETTE"
+            ],
+            [
+                "LYCEE DES ARTS ET METIERS",
+                "19, rue Guillaume Schneider",
+                "L-2522",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE DES ARTS ET METIERS - ANNEXE DOMMELDANGE",
+                "20, rue Nicolas Hein",
+                "L-1721",
+                "DOMMELDANGE"
+            ],
+            [
+                "LYCEE DU NORD",
+                "19, rue Général Patton",
+                "L-9551",
+                "WILTZ"
+            ],
+            [
+                "LYCEE EDWARD STEICHEN CLERVAUX",
+                "1, Grand-Rue",
+                "L-9710",
+                "CLERVAUX"
+            ],
+            [
+                "LYCEE ERMESINDE",
+                "45, rue de la Gare",
+                "L-7590",
+                "BERINGEN"
+            ],
+            [
+                "LYCEE HUBERT CLEMENT",
+                "2, rue Général Patton",
+                "L-4277",
+                "ESCH/ALZETTE"
+            ],
+            [
+                "LYCEE JOSY BARTHEL",
+                "2, rue Gaston Thorn",
+                "L-8268",
+                "MAMER"
+            ],
+            [
+                "LYCEE MICHEL LUCIUS",
+                "157, avenue Pasteur",
+                "L-2311",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE MICHEL-RODANGE",
+                "30, boulevard Pierre Dupong",
+                "L-1430",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE NIC-BIEVER",
+                "28, rue du Parc",
+                "L-3542",
+                "DUDELANGE"
+            ],
+            [
+                "LYCEE ROBERT-SCHUMAN",
+                "1, boulevard Emmanuel Servais",
+                "L-2535",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE TECHNIQUE AGRICOLE",
+                "72, avenue Lucien Salentiny",
+                "L-9080",
+                "ETTELBRUCK"
+            ],
+            [
+                "LYCEE TECHNIQUE DE BONNEVOIE",
+                "119, rue du Cimetière",
+                "L-1338",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE TECHNIQUE DE LALLANGE",
+                "Boulevard Hubert Clément",
+                "L-4064",
+                "ESCH-SUR-ALZETTE"
+            ],
+            [
+                "LYCEE TECHNIQUE D'ESCH-SUR-ALZETTE",
+                "32, rue Henri Koch",
+                "L-4354",
+                "ESCH-SUR-ALZETTE"
+            ],
+            [
+                "LYCEE TECHNIQUE D'ETTELBRUCK",
+                "71, av Salentiny",
+                "L-9080",
+                "ETTELBRUCK"
+            ],
+            [
+                "LYCEE TECHNIQUE DU CENTRE",
+                "106, avenue Pasteur",
+                "L-2309",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE TECHNIQUE DU CENTRE - ANNEXE KIRCHBERG",
+                "6, rue Coudenhove-Kalergi",
+                "L-1359",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE TECHNIQUE MATHIAS ADAM",
+                "Avenue de l'Europe",
+                "L-4802",
+                "LAMADELAINE"
+            ],
+            [
+                "LYCEE TECHNIQUE MATHIAS ADAM - ANNEXE JENKER",
+                "Rue Pierre Frieden",
+                "L-4560",
+                "DIFFERDANGE"
+            ],
+            [
+                "LYCEE TECHNIQUE PRIVE EMILE METZ",
+                "50, rue de Beggen",
+                "L-1220",
+                "LUXEMBOURG"
+            ],
+            [
+                "LYCEE VAUBAN",
+                "1, avenue Joseph Sax",
+                "L-2515",
+                "LUXEMBOURG"
+            ],
+            [
+                "MAACHER LYCEE",
+                "18, rue de Muenschecker",
+                "L-6760",
+                "GREVENMACHER"
+            ],
+            [
+                "NORDSTAD-LYCEE",
+                "6, rue Joseph Merten",
+                "L-9257",
+                "DIEKIRCH"
+            ],
+            [
+                "SCHENGEN-LYZEUM",
+                "Auf dem Sabel 2",
+                "D-66706",
+                "PERL"
+            ],
+            [
+                "SPORTLYCEE",
+                "66, rue de Trèves",
+                "L-2630",
+                "LUXEMBOURG"
+            ],
+            [
+                "ST GEORGE'S INTERNATIONAL SCHOOL",
+                "11, rue des Peupliers",
+                "L-2328",
+                "LUXEMBOURG"
+            ],
+        ];
+
+        foreach ($data as $single) {
+            \App\School::create([
+                'name' => $single[0],
+                'address' => $single[1],
+                'postal_code' => $single[2],
+                'city' => $single[3],
+            ]);
+        }
+
+    }
+}
