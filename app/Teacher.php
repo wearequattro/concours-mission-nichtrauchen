de615e766bb22abb7d3ce658ff207e90f19fb0e4
Gilles Braun <gilles@kirepo.lu>
2018-09-10 11:48:02 +0200

    Fix Teacher relation, add full_name update

--- app/Teacher.php
+++ app/Teacher.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
@@ -15,6 +16,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  * @property string first_name
  * @property string last_name
  * @property string phone
+ * @property string full_name
  * @property Carbon updated_at
  * @property Carbon created_at
  * @property User user
@@ -25,12 +27,19 @@ class Teacher extends Model {
 
     protected $fillable = ['first_name', 'last_name', 'phone', 'salutation_id'];
 
+    protected $appends = ['full_name'];
+
     public function user(): HasOne {
         return $this->hasOne(User::class);
     }
 
-    public function salutation(): HasOne {
-        return $this->hasOne(Salutation::class);
+    public function salutation(): BelongsTo {
+        return $this->belongsTo(Salutation::class);
+    }
+
+    public function getFullNameAttribute() {
+        $s = $this->salutation()->first()->short_form;
+        return $s . " " . $this->first_name . " " . $this->last_name;
     }
 
     /**
