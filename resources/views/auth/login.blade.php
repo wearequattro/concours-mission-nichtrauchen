13c413ebbd795a472fc8cd9302850110a3b999be
Gilles Braun <gilles@kirepo.lu>
2018-09-12 10:58:17 +0200

    Login remember checkbox no frontend validation class

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,8 +29,8 @@
 
             <div class="form-group text-left ml-4">
                 <input type="checkbox" name="remember" id="remember"
-                       class="custom-control-input">
-                <label for="remember" class="custom-control-label" {{ old('remember') != null ? 'checked' : '' }}>Se souvenir de moi</label>
+                       class="custom-control-input" {{ old('remember') != null ? 'checked' : '' }}>
+                <label for="remember" class="custom-control-label">Se souvenir de moi</label>
             </div>
 
 
