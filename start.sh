c2fcd7445f644d9ab903e702b5ec2c1747af34f7
Gilles Braun <gilles@kirepo.lu>
2018-09-24 15:33:13 +0200

    remove unused env.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "Creating env var file"
-declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /root/env.sh
-chmod +x /root/env.sh
-
 echo "Starting cron"
 cron
 
