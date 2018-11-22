5ea54ff7eabbc9441af1dece55a9fc7e9dc6334e
Gilles Braun <gilles@kirepo.lu>
2018-11-22 13:08:30 +0100

    Fix Strings in 500 error page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -460,13 +460,12 @@
 <div class="md:flex min-h-screen">
     <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
         <div class="max-w-sm m-8">
-            <div class="text-black text-5xl md:text-15xl font-black">
-                403                    </div>
+            <div class="text-black text-5xl md:text-15xl font-black">500</div>
 
             <div class="w-16 h-1 bg-purple-light my-3 md:my-6"></div>
 
             <p class="text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">
-                Une erreur c'est produite, veuillez réessayer plus tard.
+                Une erreur s'est produite, veuillez réessayer plus tard.
             </p>
 
             <a href="{{ route('login') }}">
