8dda7cf501fd301374532f161ab480d2cdd1285e
Gilles Braun <gilles@apps.lu>
2020-05-19 09:37:55 +0000

    Decrease number of backups kept

--- config/backup.php
+++ config/backup.php
@@ -187,12 +187,12 @@ return [
             /*
              * The number of days for which daily backups must be kept.
              */
-            'keep_daily_backups_for_days' => 16,
+            'keep_daily_backups_for_days' => 2,
 
             /*
              * The number of weeks for which one weekly backup must be kept.
              */
-            'keep_weekly_backups_for_weeks' => 8,
+            'keep_weekly_backups_for_weeks' => 4,
 
             /*
              * The number of months for which one monthly backup must be kept.
