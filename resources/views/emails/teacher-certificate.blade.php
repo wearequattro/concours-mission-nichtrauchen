3102c117727e1811ff2a92c97f0e26e269ae67d8
Dimitri Lebedef <dimitri@wearequattro.com>
2023-06-09 16:13:18 +0200

    MNR 2023 - Update certificat mail content

--- resources/views/emails/teacher-certificate.blade.php
+++ resources/views/emails/teacher-certificate.blade.php
@@ -1,28 +1,25 @@
 @extends('emails.layout')
 
 @section("content")
-    <p> {{$teacher->getFullNameAttribute()}}, </p>
+    <p>Chers enseignants, </p>
 
-    <p>Cet e-mail pour vous informer que l'édition 2022-2023 du concours de la Mission Nichtrauchen est officiellement terminée.</p>
-    <p>163 classes du Luxembourg avec 2973 élèves ont participé à cette édition, qui a donc été, grâce à vous, un grand succès !</p>
-    <p>Nous avons ainsi le plaisir de vous annoncer les 3 gagnants de cette année :</p>
+    <p>Cet e-mail pour vous informer que le concours Mission Nichtrauchen 2022-2023 est officiellement terminé.</p>
+    <p>La persévérance et le dévouement de vos classes en valaient la peine, et nous vous remercions pour votre participation au concours. En tout, 163 classes du Luxembourg avec 2973 élèves ont participé à cette édition, qui a donc été, grâce à vous, un grand succès !</p>
+    <p>Vous trouverez dans votre espace personnel, sous ce <a href="{{ route('certificate.page', ['uid' => $certificate->uid]) }}">lien</a>, votre certificat de participation.</p>
+    <p>Nous avons également le plaisir d'annoncer les trois classes gagnantes de cette année, que nous félicitons très chaleureusement :</p>
     <ul>
-        <li> 1<sup>er</sup> prix – 1 000 € : la classe 5CCL2 du Lycée Josy Barthel Mamer (enseignante : Mme Dominique Schmit)
-        </li>
-        <li> 2<sup>ème</sup> prix – 500 € : la classe 7C6 du Lycée Ermesinde (enseignante : Mme Justine Godziewski)
-        </li>
-        <li> 3<sup>ème</sup> prix – 250 € : la classe LNBD du Lycée Nic-Biever (enseignante : Mme Martine Linden) ex aequo avec la classe 6C3 du Lycée Josy Barthel
-            Mamer (enseignante : Mme Dominique Schmit)
-        </li>
+        <li> 1<sup>er</sup> prix – 1 000 € : la classe 4GPSAF1 du Lycée technique du Centre (enseignante : Mme Sandra Urbing)</li>
+        <li> 2<sup>ème</sup> prix – 500 € : la classe 7C3 du Lycée Josy Barthel Mamer (enseignante : Mme Dominique Schmit)</li>
+        <li> 3<sup>ème</sup> prix – 250 € : la classe 6e2 du Lycée de Garçons Esch (enseignante : Mme Sylvie Thimmesch)</li>
     </ul>
 
-    <p>La persévérance et le dévouement de vos classes en valaient la peine, et nous vous remercions d'avoir participé au concours Mission Nichtrauchen 2022-2023 !</p>
+    <p>Pour tous ceux qui ont été présents à la fête de clôture hier, qui a été une réussite éclatante grâce à votre engagement, les solutions des stations quiz sont désormais disponibles dans votre <a href="https://concours.missionnichtrauchen.lu/login" target="_blank">espace personnel</a>.</p>
 
     <p>Vous trouverez votre certificat de participation sous ce <a href="{{ route('certificate.page', ['uid' => $certificate->uid]) }}">lien</a>.</p>
 
-    <p>Ne manquez pas non plus de découvrir les photos de la fête de clôture sur le site internet : <a href="https://missionnichtrauchen.lu/medias" target="_blank">https://missionnichtrauchen.lu/medias</a></p>
+    <p>Et ne manquez pas non plus de découvrir les photos de l'événement sur le site internet : <a href="https://missionnichtrauchen.lu/medias" target="_blank">https://missionnichtrauchen.lu/medias</a></p>
 
-    <p>En attendant, nous vous souhaitons une agréable fin d'année scolaire et nous vous donnons d'ores et déjà rendez-vous pour l'édition 2023-2024 du concours Mission Nichtrauchen !</p>
+    <p>En attendant, nous vous souhaitons une agréable fin d’année scolaire et nous vous donnons d’ores et déjà rendez-vous pour l’édition 2023-2024 du concours Mission Nichtrauchen !</p>
 
     <p>Mat beschte Gréiss,</p>
     <p>D'Ekipp vun der Fondation Cancer</p>
